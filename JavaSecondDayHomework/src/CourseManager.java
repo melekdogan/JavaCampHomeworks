c7d11303332d47b593d8d4a2a02f9f541b475944
melek <melekdogand@gmail.com>
2021-04-29 17:12:57 +0300

    Java Camp Second Day Homework

--- JavaSecondDayHomework/src/CourseManager.java
+++
@@ -1,6 +0,0 @@
-
-public class CourseManager {
-  public void joinCourse(Course course) {
-	  System.out.println("Tebrikler! "+course.name+" kursuna başarıyla katıldınız!");
-  }
-}
--- JavaSecondDayHomework/src/CreditCardManager.java
+++
@@ -1,6 +0,0 @@
-
-public class CreditCardManager {
- public void addCreditCard(CreditCard creditCard){
-	 System.out.println("Tebrikler "+creditCard.cardName+"! Kredi Kartýnýz baþarýyla eklenmiþtir!");
- }
-}
