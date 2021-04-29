6da65796e01248d1017f8fc9dc6d08ee293bad55
melek <melekdogand@gmail.com>
2021-04-29 16:55:37 +0300

    deneme

---
+++ JavaSecondDayHomework/src/CreditCardManager.java
@@ -0,0 +1,6 @@
+
+public class CreditCardManager {
+ public void addCreditCard(CreditCard creditCard){
+	 System.out.println("Tebrikler "+creditCard.cardName+"! Kredi Kartýnýz baþarýyla eklenmiþtir!");
+ }
+}
