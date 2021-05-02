a2f226e614fff4df182aa23a804b313bdb0a4ecf
melek <melekdogand@gmail.com>
2021-05-02 19:10:11 +0300

    Second Day Homework...

---
+++ JavaSecondDayHomework/src/Course.java
@@ -0,0 +1,13 @@
+
+public class Course {
+	int id;
+	String name;
+	String teacher;
+	
+	public  Course(int id, String name, String teacher) {
+		this.id=id;
+		this.name=name;
+		this.teacher=teacher;
+	}
+
+}
---
+++ JavaSecondDayHomework/src/CourseManager.java
@@ -0,0 +1,6 @@
+
+public class CourseManager {
+  public void joinCourse(Course course) {
+	  System.out.println("Tebrikler! "+course.name+" kursuna başarıyla katıldınız!");
+  }
+}
